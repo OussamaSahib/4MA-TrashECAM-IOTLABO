596a3ebd566bcb0d6537269de91a64113e25ec6b
Oussama <195052@ecam.be>
2024-12-08 22:16:59 +0100

    TRASHMANAGER: Button SEND JSON +Values Weight and Quantity +CREATION OF HOMEPAGE AND TRASHCOLLECTOR

--- routes.js
+++ routes.js
@@ -3,7 +3,17 @@ let router=express.Router();
 
 router.get("/", (req,res)=>
 {
+    res.render("Home.ejs");
+});
+
+// Route vers TrashManager.ejs
+router.get("/trashmanager", (req, res) => {
     res.render("TrashManager.ejs");
+});
+
+// Route vers Eboueur.ejs
+router.get("/trashcollector", (req, res) => {
+    res.render("TrashCollector.ejs");
 });
 
 //EXPORT DU ROUTER
--- server.js
+++ server.js
@@ -33,23 +33,24 @@ client.on("connect",()=> {
 
 //RECUPERATION DATAS DU TTN QU'ON ENREGISTRE DS JSON
 //JSON: Liste [], contenant plusieurs Dict. {}(=chaque poubelle)
-function SaveData(ID, bytes, latitude, longitude) {
+function SaveData(ID, weight,  quantity, latitude, longitude) {
   //Données
   const newTrashData= {
     "ID": ID,
-    "Bytes": bytes,
+    "Weight": weight,
+    "Quantity": quantity,
     "Latitude": latitude,
     "Longitude": longitude,
   };
   fs.readFile("./public/trashs.json", "utf8", (err, data)=> {
     let jsonData= [];
-    if (!err) {
-      try {
+    if (!err){
+      try{
         jsonData= JSON.parse(data);
         if (!Array.isArray(jsonData)) {
           jsonData= [];
         }
-      } catch (parseError) {
+      } catch (parseError){
         console.error("Erreur de parsing JSON:", parseError);
         jsonData= [];
       }
@@ -57,11 +58,12 @@ function SaveData(ID, bytes, latitude, longitude) {
 
     //Mise à jour des données
     const existingIndex= jsonData.findIndex(trash=> trash.ID===ID);
-    if (existingIndex>=0) {
-      jsonData[existingIndex].Bytes= bytes;
+    if (existingIndex>=0){
+      jsonData[existingIndex].Weight= weight;
+      jsonData[existingIndex].Quantity= quantity;
       jsonData[existingIndex].Latitude= latitude;
       jsonData[existingIndex].Longitude= longitude;
-    } else {
+    } else{
       jsonData.push(newTrashData);
     }
     fs.writeFile("./public/trashs.json", JSON.stringify(jsonData, null, 4), (err)=> {
@@ -79,15 +81,19 @@ client.on("message", (topic, message)=> {
   console.log("Données reçues de TTN:", jsonData);
 
   const ID= jsonData.end_device_ids.device_id;
-  const Bytes= jsonData.uplink_message.decoded_payload.bytes;
+  const Weight= jsonData.uplink_message.decoded_payload.weight;
+  const Quantity= jsonData.uplink_message.decoded_payload.quantity;
   const Latitude= jsonData.uplink_message.locations.user.latitude;
   const Longitude= jsonData.uplink_message.locations.user.longitude;
 
   console.log("ID:", ID);  
   io.emit("event-ID", ID);
 
-  console.log("Bytes:", Bytes);  
-  io.emit("event-bytes", Bytes);
+  console.log("Weight:", Weight);  
+  io.emit("event-weight", Weight);
+
+  console.log("Quantity:", Quantity);  
+  io.emit("event-quantity", Quantity);
 
   console.log("Latitude:", Latitude)
   io.emit("event-latitude", Latitude);
@@ -95,8 +101,50 @@ client.on("message", (topic, message)=> {
   console.log("Longitude:", Longitude)
   io.emit("event-longitude", Longitude);
 
-  SaveData(ID, Bytes, Latitude, Longitude);
+  SaveData(ID, Weight, Quantity, Latitude, Longitude);
 });
+
+
+
+
+//ENVOIE DATAS ROUTE TO TRASHROUTES.JSON
+app.use(express.json()); 
+app.post("/saveRoute", (req, res)=> {
+  const routeData= req.body;
+  const filePath= "./public/trashsroutes.json";
+
+
+  fs.readFile(filePath, "utf8", (err, data)=> {
+      if (err){
+          console.error("Error while reading JSON file: ", err);
+          res.status(500).send("Error while reading JSON file: ");
+          return;
+      }
+
+      let jsonData= [];
+      try{
+          jsonData= JSON.parse(data);
+      } 
+      catch (parseError){
+          console.error("JSON parsing error: ", parseError);
+      }
+
+      //News Datas
+      jsonData.push(routeData);
+
+      fs.writeFile(filePath, JSON.stringify(jsonData, null, 4), (writeErr)=> {
+          if (writeErr){
+              console.error("Error while writing JSON file: ", writeErr);
+              res.status(500).send("Error while writing JSON file: ");
+          } 
+          else{
+              res.status(200).send("Datas successfully saved");
+          }
+      });
+  });
+});
+
+
 
 
 //METHODE LISTEN
